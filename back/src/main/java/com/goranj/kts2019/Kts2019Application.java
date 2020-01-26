05bd34947a857b1c21f58ae5a4a905b7da78e83f
Goran Jankovic <jan.goran@outlook.com>
2020-01-26 14:56:28 +0100

    init commit

---
+++ back/src/main/java/com/goranj/kts2019/Kts2019Application.java
@@ -0,0 +1,13 @@
+package com.goranj.kts2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kts2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kts2019Application.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/goranj/kts2019/Kts2019ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goranj.kts2019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kts2019ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
